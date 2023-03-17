9846415874da4fdec6d0c565a86b86ca57f0c955
asswwa <60053830+asswwa@users.noreply.github.com>
2023-03-17 21:18:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let ass = [
+    'украина',
+    'я хохол',
+    'я правнук гитлера',
+    'я гитлер',
+    'я еврей',
+    'письки',
+    'жопы',
+    'мама ярика',
+    '11 сентября',
+    'садам хусейн',
+    'тесак',
+    'ракеты на украину',
+    'дети',
+    'сталин',
+    'свастига',
+    'зига',
+    'басиста не слышно',
+    'вар тандер',
+]
+
+document.querySelector("#hoha").onclick = function(){
+    let joke = Math.floor(Math.random() * ((ass.length - 1) + 1));
+    document.getElementById("demo").innerHTML = ass[joke];
+
+}
